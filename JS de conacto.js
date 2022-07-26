bb8ea2110ee803ec0948647960f99841c10a6ac0
Rootex-dot <78125808+Rootex-dot@users.noreply.github.com>
2022-07-26 16:38:40 -0500

    ini

---
+++ JS de conacto.js
@@ -0,0 +1,15 @@
+//Declaración del h1 para poner el slogan de hemodialisis.
+let typed = new Typed('#typed', {
+    strings:[ 'La calidad de la atención ^1000', 'Reflejada en un precio justo', 'Centro de Atención de especialidades navales y Hemodiálisis.'],
+    startDelay: 500, //500
+    typeSpeed:50, //50
+    backSpeed: 70, //70
+    backDelay: 700, //700
+    loop: false,
+    cursorChar: '+'
+  });
+  //^1000
+
+  //Corregir fallo de la escritura del titulo
+
+  
---
+++ main.js
@@ -0,0 +1,9 @@
+let typed = new Typed('#typed', {
+    strings:[  '¿Tus riñones estan en peligro? ¡Aún se pueden salvar!'],
+    startDelay: 500, //500
+    typeSpeed:50, //50
+    backSpeed: 70, //70
+    backDelay: 700, //700
+    loop: false,
+    cursorChar: '+'
+  });
